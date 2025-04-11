5e8722d6b6ac1a636a8a4183e22f42af12c97707
Santiago <andresnavarro3@hotmail.com>
2025-04-11 13:55:56 -0500

    added ESLint, added prettier, package.json cleaned, removed testing dependencies, removed unused scripts cleaned index.js

--- src/Hooks/useAuth.js
+++ src/Hooks/useAuth.js
@@ -1,23 +1,23 @@
 // src/hooks/useAuth.js
-import { useEffect, useState } from 'react';
-import { jwtDecode } from 'jwt-decode';
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
-const  useAuth = () => {
+const useAuth = () => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       try {
         const decoded = jwtDecode(token);
         setUser(decoded);
       } catch (error) {
         console.error("Invalid token:", error);
-        localStorage.removeItem('token'); // Clean up invalid token
+        localStorage.removeItem("token"); // Clean up invalid token
       }
     }
   }, []);
 
   return user;
-}
+};
 export default useAuth;
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,97 +1,102 @@
-  import logo from '.././logo.svg';
-  import Knuckles from '.././Images/Knuckles.jpg';
-  import './HomePage.css';
-  import { useState, useEffect } from 'react';
-  import { Link } from 'react-router-dom';
-  import useAuth from '../Hooks/useAuth';
+import logo from ".././logo.svg";
+import Knuckles from ".././Images/Knuckles.jpg";
+import "./HomePage.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import useAuth from "../Hooks/useAuth";
 
-  function HomePage() {
-    const user = useAuth();
-    const [currentTime, setCurrentTime] = useState(new Date());
+function HomePage() {
+  const user = useAuth();
+  const [currentTime, setCurrentTime] = useState(new Date());
 
-    useEffect(() => {
-      const timer = setInterval(() => {
-        setCurrentTime(new Date());
-      }, 1000); // Update every second
-      // Clean up the interval on component unmount
-      return () => clearInterval(timer);
-    }, []);  // Empty dependency array to run this effect only once after mount
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000); // Update every second
+    // Clean up the interval on component unmount
+    return () => clearInterval(timer);
+  }, []); // Empty dependency array to run this effect only once after mount
 
-    //////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
 
-    const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
-      fetch("https://lg4vp1wifk.execute-api.us-east-2.amazonaws.com/default/PropertyManagementBackend")
-        .then(response => response.json())  // ✅ Now this will work
-        .then(data => setMessage(data.message)) // ✅ Extract message
-        .catch(error => console.error("Error fetching data:", error));
-    }, []);
+  useEffect(() => {
+    fetch(
+      "https://lg4vp1wifk.execute-api.us-east-2.amazonaws.com/default/PropertyManagementBackend",
+    )
+      .then((response) => response.json()) // ✅ Now this will work
+      .then((data) => setMessage(data.message)) // ✅ Extract message
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
 
-    //////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
+  return (
+    <header className="App-header">
+      <h1>Bienvenido a las Casas</h1>
+      <div>
+        <Link to="/Login">
+          <button>Log In</button>
+        </Link>
+        <Link to="/house/1">
+          <button>Casa 1</button>
+        </Link>
+        <Link to="/house/2">
+          <button>Casa 2</button>
+        </Link>
+        <Link to="/house/3">
+          <button>Casa 3</button>
+        </Link>
+      </div>
+      <div>
+        {user ? (
+          <>
+            <p className="xd"> Como estas {user.userName}!</p>
+            <p className="xd">Eres {user.role}</p>
+          </>
+        ) : (
+          <p>Please log in to see your content.</p>
+        )}
+      </div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        <h2>
+          Today is {currentTime.toLocaleDateString()} and the time now is{" "}
+          {currentTime.toLocaleTimeString()}.
+        </h2>
+      </p>
+      <div>
+        <h1>API Response:</h1>
+        <p className="lambda">{message || "Loading..."}</p>
+      </div>
 
+      {/* Knuckles Image Below the Slider */}
+      <div className="Knuckles">
+        <img
+          src={Knuckles}
+          className="Knuckles-img"
+          alt="Ugandan"
+          width={300}
+          height={300}
+        />
+      </div>
 
-    return (
-          <header className="App-header">
-            <h1>Bienvenido a las Casas</h1>
-            <div>
-              <Link to ="/Login">
-                <button>Log In</button>
-              </Link>
-            <Link to="/house/1">
-                <button>Casa 1</button>
-            </Link>
-            <Link to="/house/2">
-                <button>Casa 2</button>
-            </Link>
-            <Link to="/house/3">
-                <button>Casa 3</button>
-            </Link>
-            </div>
-            <div>{user?(
-              <>
-              <p className='xd'> Como estas {user.userName}!</p>
-              <p className='xd'>Eres {user.role}</p>
-              </>
-            ):(
-              <p>Please log in to see your content.</p>
-            )}</div>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <h2>Today is {currentTime.toLocaleDateString()} and the time now is {currentTime.toLocaleTimeString()}.</h2>
-          </p>
-          <div>
-            <h1>API Response:</h1>
-            <p className='lambda'>{message || "Loading..."}</p>
-          </div>
-          
-          {/* Knuckles Image Below the Slider */}
-          <div className="Knuckles">
-            <img
-              src={Knuckles}
-              className="Knuckles-img"
-              alt="Ugandan"
-              width={300}
-              height={300}
-            />
-          </div>
-    
-          {/* Learn React Link */}
-          <a
-            className="App-link"
-            href="https://www.youtube.com/watch?v=NhKVUFVCjnA&ab_channel=BAUTIELCRACK"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-    );
-  }
+      {/* Learn React Link */}
+      <a
+        className="App-link"
+        href="https://www.youtube.com/watch?v=NhKVUFVCjnA&ab_channel=BAUTIELCRACK"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+}
 
-  export default HomePage;
+export default HomePage;
